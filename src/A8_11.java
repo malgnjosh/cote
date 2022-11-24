54a424684d9c04070f417fab21d82b1a75c2f76b
malgnjosh <josh@malgnsoft.com>
2022-11-24 22:49:30 +0900

    1124

---
+++ src/A8_11.java
@@ -0,0 +1,71 @@
+import java.util.*;
+
+public class A8_11 {
+    static int[] dx={-1,0,1,0};
+    static int[] dy={0,1,0,-1};
+    static int[][] board, dis;
+    public void BFS(int x, int y) {
+        Queue<CoordinateXY> q = new LinkedList<>();
+        q.offer(new CoordinateXY(1, 1));
+        board[x][y] = 1;
+        while(!q.isEmpty()) {
+            CoordinateXY tmp = q.poll();
+            for(int i = 0; i < 4; i++) {
+                int nx = tmp.getX() + dx[i];
+                int ny = tmp.getY() + dy[i];
+                if(nx >= 1 && nx <= 7 && ny >= 1 && ny <= 7 && board[nx][ny] == 0) {
+                    board[nx][ny] = 1;
+                    q.offer(new CoordinateXY(nx, ny));
+                    dis[nx][ny] = dis[tmp.getX()][tmp.getY()] + 1;
+                }
+            }
+        }
+    }
+    public static void main(String[] args) {
+        A8_11 T = new A8_11();
+        Scanner sc = new Scanner(System.in);
+        board = new int[8][8];
+        dis = new int[8][8];
+
+        //pos = new CoordinateXY(1, 1);
+
+        for(int i = 1; i <= 7; i++) {
+            for(int j = 1; j <= 7; j++) {
+                board[i][j] = sc.nextInt();
+            }
+        }
+        T.BFS(1, 1);
+        if(dis[7][7] == 0) System.out.println(-1);
+        else System.out.println(dis[7][7]);
+
+    }
+}
+
+class CoordinateXY {
+    private int x, y;
+    CoordinateXY(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public void setXY(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
--- src/Q8_10.java
+++ src/Q8_10.java
@@ -1,125 +1,97 @@
-import java.util.*;
-
-public class Q8_10 {
-    public static final int MAX = 7;
-    public static int result = 0;
-    public static CoordinateXY pos;
-
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int[][] board = new int[MAX + 1][MAX + 1];
-
-        pos = new CoordinateXY(1, 1);
-
-        for(int i = 1; i < MAX + 1; i++) {
-            board[i][0] = 2; // 벽
-            for(int j = 1; j < MAX + 1; j++) {
-                board[i][j] = sc.nextInt();
-                if(i == 1) board[0][j] = 2;
-            }
-        }
-        board[0][0] = 2;
-        board[1][1] = -1;
-
-        // 출력
-        for(int[] bo : board) {
-            for(int b : bo) {
-                System.out.print(b + " ");
-            }
-            System.out.println();
-        }
-
-        findPath(board, pos);
-        System.out.println(result);
-    }
-
-    public static boolean findPath(int[][] board, CoordinateXY pos) {
-        int[][] temp = new int[MAX + 1][MAX + 1];
-        arrayCopy(board, temp);
-
-        int cnt = 0;
-        if(pos.getX() == 7 && pos.getY() == 7) {
-            result++;
-            return true;
-        }
-
-        for(int i = 0; i < 4; i++) {
-
-            if(searchNext(board, pos))
-        }
-        return false;
-    }
-
-    public static boolean moveBack(int[][] board, int[] pos) {
-        boolean ret = true;
-
-        if(pos[0] == 7 && pos[1] == 7) { return false; }
-
-        board[pos[0]][pos[1]] = -1; // 현재위치
-        if(pos[0] + 1 <= MAX && board[pos[0] + 1][pos[1]] == -1) pos[0]++;
-        else if(pos[0] - 1 <= MAX && board[pos[0] - 1][pos[1]] == -1) pos[0]--;
-        else if(pos[1] + 1 <= MAX && board[pos[0]][pos[1] + 1] == -1) pos[1]++;
-        else if(pos[1] - 1 <= MAX && board[pos[0]][pos[1] - 1] == -1) pos[1]--;
-        else ret = false;
-
-        return ret;
-    }
-    public static boolean moveNext(int[][] board, CoordinateXY from, String direction) {
-        CoordinateXY to;
-        switch(direction) {
-            case "left": to = new CoordinateXY(from.getY() - 1, from.getX());
-            case "right": to = new CoordinateXY(from.getY() + 1, from.getX());
-            case "up": to = new CoordinateXY(from.getY(), from.getX() - 1);
-            case "down": to = new CoordinateXY(from.getY(), from.getX() + 1);
-        }
-
-
-        if(searchNext(board, to)
-    }
-    public static boolean searchNext(int[][] board, CoordinateXY from, String direction) {
-        CoordinateXY to;
-        switch(direction) {
-            case "left": to = new CoordinateXY(from.getY() - 1, from.getX());
-            case "right": to = new CoordinateXY(from.getY() + 1, from.getX());
-            case "up": to = new CoordinateXY(from.getY(), from.getX() - 1);
-            case "down": to = new CoordinateXY(from.getY(), from.getX() + 1);
-        }
-
-        return to.getX() <= MAX && to.getY() <= MAX && board[to.getY()][to.getX()] == 0;
-    }
-
-    public static void arrayCopy(int[][] A, int[][] B) {
-        for (int i = 0; i < A.length; i++) {
-            System.arraycopy(A[i], 0, B[i], 0, A[i].length);
-        }
-    }
-}
-
-class CoordinateXY {
-    private int x, y;
-    CoordinateXY(int x, int y) {
-        this.x = x;
-        this.y = y;
-    }
-
-    public int getX() {
-        return x;
-    }
-
-    public int getY() {
-        return y;
-    }
-
-    public void setX(int x) {
-        this.x = x;
-    }
-
-    public void setY(int y) {
-        this.y = y;
-    }
-
-    public void setXY(int x, int y) {
-        this.x = x;
-        this.y = y;
-    }
-}
+//import java.util.*;
+//
+//public class Q8_10 {
+//    public static final int MAX = 7;
+//    public static int result = 0;
+//    public static CoordinateXY pos;
+//
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        int[][] board = new int[MAX + 1][MAX + 1];
+//
+//        pos = new CoordinateXY(1, 1);
+//
+//        for(int i = 1; i < MAX + 1; i++) {
+//            board[i][0] = 2; // 벽
+//            for(int j = 1; j < MAX + 1; j++) {
+//                board[i][j] = sc.nextInt();
+//                if(i == 1) board[0][j] = 2;
+//            }
+//        }
+//        board[0][0] = 2;
+//        board[1][1] = -1;
+//
+//        // 출력
+//        for(int[] bo : board) {
+//            for(int b : bo) {
+//                System.out.print(b + " ");
+//            }
+//            System.out.println();
+//        }
+//
+//        findPath(board, pos);
+//        System.out.println(result);
+//    }
+//
+//    public static boolean findPath(int[][] board, CoordinateXY pos) {
+//        int[][] temp = new int[MAX + 1][MAX + 1];
+//        arrayCopy(board, temp);
+//
+//        int cnt = 0;
+//        if(pos.getX() == 7 && pos.getY() == 7) {
+//            result++;
+//            return true;
+//        }
+//
+//        for(int i = 0; i < 4; i++) {
+//
+//         //   if(searchNext(board, pos))
+//        }
+//        return false;
+//    }
+//
+//    public static boolean moveBack(int[][] board, int[] pos) {
+//        boolean ret = true;
+//
+//        if(pos[0] == 7 && pos[1] == 7) { return false; }
+//
+//        board[pos[0]][pos[1]] = -1; // 현재위치
+//        if(pos[0] + 1 <= MAX && board[pos[0] + 1][pos[1]] == -1) pos[0]++;
+//        else if(pos[0] - 1 <= MAX && board[pos[0] - 1][pos[1]] == -1) pos[0]--;
+//        else if(pos[1] + 1 <= MAX && board[pos[0]][pos[1] + 1] == -1) pos[1]++;
+//        else if(pos[1] - 1 <= MAX && board[pos[0]][pos[1] - 1] == -1) pos[1]--;
+//        else ret = false;
+//
+//        return ret;
+//    }
+//    public static boolean moveNext(int[][] board, CoordinateXY from, String direction) {
+//        CoordinateXY to;
+//        switch(direction) {
+//            case "left": to = new CoordinateXY(from.getY() - 1, from.getX());
+//            case "right": to = new CoordinateXY(from.getY() + 1, from.getX());
+//            case "up": to = new CoordinateXY(from.getY(), from.getX() - 1);
+//            case "down": to = new CoordinateXY(from.getY(), from.getX() + 1);
+//        }
+//
+//
+//        //if(searchNext(board, to)
+//    }
+//    public static boolean searchNext(int[][] board, CoordinateXY from, String direction) {
+//        CoordinateXY to;
+//        switch(direction) {
+//            case "left": to = new CoordinateXY(from.getY() - 1, from.getX());
+//            case "right": to = new CoordinateXY(from.getY() + 1, from.getX());
+//            case "up": to = new CoordinateXY(from.getY(), from.getX() - 1);
+//            case "down": to = new CoordinateXY(from.getY(), from.getX() + 1);
+//        }
+//
+//        return to.getX() <= MAX && to.getY() <= MAX && board[to.getY()][to.getX()] == 0;
+//    }
+//
+//    public static void arrayCopy(int[][] A, int[][] B) {
+//        for (int i = 0; i < A.length; i++) {
+//            System.arraycopy(A[i], 0, B[i], 0, A[i].length);
+//        }
+//    }
+//}
+//
